a9d2180ca18e6bdeb5362918fba0f58674ea5f58
MT <michaltarski@gmail.com>
2025-07-15 10:09:10 +0200

    playwright test modified

--- playwright/test_login_playwright.py
+++ playwright/test_login_playwright.py
@@ -7,8 +7,8 @@ def test_login_playwright(page: Page):
 
     page.goto(URL)
 
-    page.locator("[data-test=\"username\"]").fill("standard_user")
-    page.locator("[data-test=\"password\"]").fill("secret_sauce")
+    page.locator("[data-test='username']").fill("standard_user")
+    page.locator("[data-test='password']").fill("secret_sauce")
     page.get_by_role("button", name="LOGIN").click()
 
     expect(page).to_have_url("https://www.saucedemo.com/v1/inventory.html")
@@ -19,10 +19,10 @@ def test_add_products_to_the_cart(page: Page):
 
     page.goto(URL)
 
-    page.locator("[data-test=\"username\"]").click()
-    page.locator("[data-test=\"username\"]").fill("standard_user")
-    page.locator("[data-test=\"password\"]").click()
-    page.locator("[data-test=\"password\"]").fill("secret_sauce")
+    page.locator("[data-test='username']").click()
+    page.locator("[data-test='username']").fill("standard_user")
+    page.locator("[data-test='password']").click()
+    page.locator("[data-test='password']").fill("secret_sauce")
     page.get_by_role("button", name="LOGIN").click()
 
     page.locator("div").filter(has_text=re.compile(r"^\$29\.99ADD TO CART$")).get_by_role("button").click()
